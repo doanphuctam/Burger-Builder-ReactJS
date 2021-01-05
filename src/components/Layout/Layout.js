2c36df2c11be53b04356a44b1e2acb56a9b63458
Tam Doan <doanphuctam@gmail.com>
2021-01-05 19:23:52 +1100

    Add SideDrawer toggle btn

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+class Layout extends Component {
+    state = {
+        showSideDrawer: false,
+    }
 
-const layout = (props) => (
-    <Aux>
-        <SideDrawer />
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler} />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
